49b45d1440a8819d16ea94d63a6f2fe7f0190024
Bry Cox <carl.cc@gmail.com>
2019-10-31 05:37:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
 // Your code goes here
 
-document.addEventListener("DOMContentLoaded", function() {
-  let element = document.getElementById("text");
-  element.innerHTML = 'This is really cool!';
-});
+
